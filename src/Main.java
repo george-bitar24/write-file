43dae172ce48c6bee036e461e7562f6fea0f7f9d
George-Fatouh Al Bitar <falbitar@cognity.gr>
2019-07-24 14:06:40 +0300

    create writer on txt

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         File file = new File("text.txt");
-        FileWriter fileWriter = new FileWriter(file);
+//
+//
+
+
+
+
+        FileWriter fileWriter = new FileWriter(file,true);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         String input = "hello malaka";
         String input2 = "hello mounoskylo";
+        bufferedWriter.newLine();
 
         bufferedWriter.write(input);
 
